7cd62342c8a823cbc1417a177a42efc69b7e32ac
Blake <finalyetifive@gmail.com>
2020-03-02 01:21:14 +0000

    End of Chapter 1

--- Code/Chapter_01/Hello_CS/Program.cs
+++ Code/Chapter_01/Hello_CS/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+using System;
 
 namespace Hello_CS
 {
     class Program
     {
+        private int Test_Integer = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, C#!");
+
+            Program_End();
+        }
+
+        static void Program_End()
+        {
+            Console.WriteLine("\nPress any key to continue . . .");
+
+            Console.ReadLine();
         }
     }
 }
